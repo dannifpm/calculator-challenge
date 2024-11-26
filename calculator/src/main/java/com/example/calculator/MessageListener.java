6f0713597c99cb333e55d21fa708a23bd2587fcc
Daniel Monteiro <>
2024-11-26 13:12:42 +0000

    Remove unused CalculatorService dependency from MessageListener

--- calculator/src/main/java/com/example/calculator/MessageListener.java
+++ calculator/src/main/java/com/example/calculator/MessageListener.java
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageListener {
 
-    public MessageListener(CalculatorService calculatorService) {
-    }
-
     @RabbitListener(queues = "calculatorQueue")
     public void processMessage(String message) {
         System.out.println("Message received: " + message);
